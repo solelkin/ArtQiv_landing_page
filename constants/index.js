32d9affa606d407b1be1c55f1ba4c7074c2b0902
Sol Elkin <selkin7373@gmail.com>
2023-09-16 19:50:45 -0700

    first commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/fvck_01.jpg',
+    title: 'Fvckrender',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/aeforia.jpg',
+    title: 'Aeforia',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/baeige.jpg',
+    title: 'Baeige',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/beeple_01.jpg',
+    title: 'Beeple',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/vacads.jpg',
+    title: '',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Add the Artists work to your archive',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Archive Engine',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/beeple_01.jpg',
+    title: 'The rise of Beeples everydays',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/dreams1.jpg',
+    title: 'The Extended realities being explored with Eduard Ov',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/fvck_01.jpg',
+    title: 'The man who showed the world how to not give a FVCK',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
